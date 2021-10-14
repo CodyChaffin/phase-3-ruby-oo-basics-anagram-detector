469a2b4d22a1e9e01fb9875a3fd02b1a7915b611
cody <cmchaff14@gmail.com>
2021-10-13 21:41:40 -0500

    commit tree f61031565fe65c2c822c739d9528ca45a61b7688

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+    end
+    def match(array)
+        match = array.select{|e| e.chars.sort == @name.chars.sort}
+        match
+    end
+    
+end
+
